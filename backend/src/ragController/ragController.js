9b107f1b10b1bfc1eee4c1f001f0e4010c358b67
felix-abdus <abduselamgena@gmail.com>
2026-09-22 01:26:32 +0300

    logic for process doc

--- backend/src/ragController/ragController.js
+++ backend/src/ragController/ragController.js
@@ -6,3 +6,23 @@ import {
   askDocument,
 } from "./ragService.js";
 
+// Upload and Process Documents
+
+export const uploadAndProcessDocument = async (req, res) => {
+  const userId = req.user.id || req.user.userId;
+  const file = req.file;
+
+  if (!file) {
+    return res.status(400).json({
+      msg: "Please select a valid PDF file.",
+    });
+  }
+  try {
+    const result = await processDocument({
+      userId,
+      file,
+    });
+
+    return res.status(201).json(result);
+  } catch (error) {}
+};
