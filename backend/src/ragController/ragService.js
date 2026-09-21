dc0c6e6f294e7544eb082dd7ad20c356c18c9d5f
felix-abdus <abduselamgena@gmail.com>
2026-09-21 23:44:07 +0300

    import important functions

---
+++ backend/src/ragController/ragService.js
@@ -0,0 +1,9 @@
+import { readPdfFile, extractPdfPages, deletePdfFile } from "./pdfService.js";
+
+import { createChunks } from "./chunkService.js";
+
+import { createEmbedding } from "./embeddingService.js";
+
+import { safeExecute } from "../../db/config.js";
+
+import { generateGroundedAnswer } from "./aiService.js";
