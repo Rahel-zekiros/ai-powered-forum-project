ad2bac5f6cc7a8199c2c0dd58d4210a2958a8107
e-besh <enasbeshir2025@gmail.com>
2026-09-22 11:04:33 +0300

    task-25: checking

--- backend/src/api/Routes/rag.Routes.js
+++ backend/src/api/Routes/rag.Routes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { authenticateUser } from "../../middleware/authentication";
+import { listDocumentsController } from "../../ragController/rag.Controller";
 const router = express.Router();
 
 // *==== list document:GET /api/rag/documents ======
