19ed3b7419627348b88cf3109713828a4ce06205
e-besh <enasbeshir2025@gmail.com>
2026-09-22 11:21:03 +0300

    task-26:add rag validation page

--- backend/src/api/Routes/rag.Routes.js
+++ backend/src/api/Routes/rag.Routes.js
@@ -1,11 +1,21 @@
 import express from "express";
 import { authenticateUser } from "../../middleware/authentication";
-import { listDocumentsController } from "../../ragController/rag.Controller";
+import {
+  listDocumentsController,
+  deleteDocumentController,
+} from "../../ragController/rag.Controller";
+import { documentIdParamValidation } from "../../ragController/rag.validation";
+
 const router = express.Router();
 
 // *==== list document:GET /api/rag/documents ======
-router.get("/document", authenticateUser, listDocumentsController);
+router.get("/documents", authenticateUser, listDocumentsController);
 
 // * ======= DELETE /api/rag/documents/:documentId ====
-router.delete("/document/:documentId", authenticateUser);
+router.delete(
+  "/document/:documentId",
+  authenticateUser,
+  documentIdParamValidation,
+  deleteDocumentController,
+);
 export default router;
